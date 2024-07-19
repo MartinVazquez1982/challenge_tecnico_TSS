75bc63df9897163f78498f5a6f0a5ae0ce9e4f4c
MartinVazquez1982 <martin.vazquezaris@gmail.com>
2024-07-19 15:25:42 -0300

    :sparkles: cross method implementation finished

--- strategies/cross_method.py
+++ strategies/cross_method.py
@@ -4,37 +4,68 @@ class CrossMethod(bt.Strategy):
 
     params = (
         ('period', 10),
+        ('risk_fraction', 0.10),
     )
 
     def __init__(self):
-        self.sma = {assets: bt.indicators.SimpleMovingAverage(self.getdatabyname(assets).close, period=self.params.period) for assets in self.getdatanames() }
-        self.positions_strategy = {assets: 0 for assets in self.getdatanames() }
-        
+        self.sma = { assets: bt.indicators.SimpleMovingAverage(self.getdatabyname(assets).close, period=self.params.period) for assets in self.getdatanames() }
+        self.positions_strategy = { assets: 0 for assets in self.getdatanames() }
+    
+    def get_size_buy_order(self, market_data) -> int:
+        """_summary_
+
+        Args:
+            market_data (_type_): _description_
+
+        Returns:
+            int: _description_
+        """
+        money = self.broker.getvalue() * self.params.risk_fraction
+        if money > self.broker.get_cash(): return 0
+        return int ( money / market_data.close[0])
 
     def next(self):
-        for assets in self.getdatanames():
-            data = self.getdatabyname(assets)
-            if self.positions_strategy[assets] == 0 and self.sma[assets][0] < data.close[0]:
-                self.buy(data=assets)
+        """_summary_
+        """
+        for assets in self.datas:
+            if self.positions_strategy[assets._name] == 0 and self.sma[assets._name][0] < assets.close[0]:
+                size_order = self.get_size_buy_order(assets)
+                if size_order > 0:
+                    order = self.buy(data=assets._name, size=size_order)
+                    order.addinfo(strategy=self)
             
-            elif self.positions_strategy[assets] > 0 and self.sma[assets][0] > data.close[0]:
-                self.sell(data=assets)
+            elif self.positions_strategy[assets._name] > 0 and self.sma[assets._name][0] > assets.close[0]:
+                order = self.sell(data=assets, size=self.positions_strategy[assets._name])
+                order.addinfo(strategy=self)
 
             
     def notify_order(self, order):
+        """_summary_
+
+        Args:
+            order (_type_): _description_
+        """
         if order.status in [order.Submitted, order.Accepted]:
             # La orden ha sido enviada o aceptada - no se necesita acción adicional
             return
 
         if order.status in [order.Completed]:
             if order.isbuy():
-                print(f'COMPRA COMPLETADA, Orden ID: {order.ref}, Asset {order.data._name}')
-                self.positions_strategy[order.data._name] += 1
+                print(f'COMPRA COMPLETADA, Orden ID: {order.ref}, Asset {order.data._name}, Size {order.size}, Strategy {order.info.strategy}')
+                self.positions_strategy[order.data._name] += order.size
             elif order.issell():
-                print(f'VENTA COMPLETADA, Orden ID: {order.ref}, Asset {order.data._name}')
+                print(f'VENTA COMPLETADA, Orden ID: {order.ref}, Asset {order.data._name}, Size {order.size}, Strategy {order.info.strategy}')
                 self.positions_strategy[order.data._name] = 0
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
             print(f'ORDEN FALLIDA, Orden ID: {order.ref}')
 
         # Limpiar la referencia de la orden después de que se ha completado o fallado
         self.order = None
+    
+    def __str__(self) -> str:
+        """_summary_
+
+        Returns:
+            str: _description_
+        """
+        return f"Cross Method - Period {self.params.period}"
