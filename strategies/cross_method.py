b97a4a8504faed29cf2614143faa34bb747d65dd
MartinVazquez1982 <martin.vazquezaris@gmail.com>
2024-07-19 17:56:26 -0300

    :recycle: creation of the conditions_buy and conditions_sell methods

--- strategies/cross_method.py
+++ strategies/cross_method.py
@@ -23,18 +23,34 @@ class CrossMethod(bt.Strategy):
         money = self.broker.getvalue() * self.params.risk_fraction
         if money > self.broker.get_cash(): return 0
         return int ( money / market_data.close[0])
+    
+    def conditions_buy(self, market_data) -> bool:
+        """_summary_
+
+        Returns:
+            bool: _description_
+        """
+        return self.sma[market_data._name][0] < market_data.close[0]
+    
+    def conditions_sell(self, market_data) -> bool:
+        """_summary_
+
+        Returns:
+            bool: _description_
+        """
+        return self.sma[market_data._name][0] > market_data.close[0]
 
     def next(self):
         """_summary_
         """
         for assets in self.datas:
-            if self.positions_strategy[assets._name] == 0 and self.sma[assets._name][0] < assets.close[0]:
+            if self.positions_strategy[assets._name] == 0 and self.conditions_buy(assets):
                 size_order = self.get_size_buy_order(assets)
                 if size_order > 0:
                     order = self.buy(data=assets._name, size=size_order)
                     order.addinfo(strategy=self)
             
-            elif self.positions_strategy[assets._name] > 0 and self.sma[assets._name][0] > assets.close[0]:
+            elif self.positions_strategy[assets._name] > 0 and self.conditions_sell(assets):
                 order = self.sell(data=assets, size=self.positions_strategy[assets._name])
                 order.addinfo(strategy=self)
 
@@ -46,20 +62,27 @@ class CrossMethod(bt.Strategy):
             order (_type_): _description_
         """
         if order.status in [order.Submitted, order.Accepted]:
-            # La orden ha sido enviada o aceptada - no se necesita acción adicional
+            # La orden ha sido enviada o aceptada - no se necesita ninguna acción adicional
             return
 
-        if order.status in [order.Completed]:
+        if order.status == order.Completed:
             if order.isbuy():
                 print(f'COMPRA COMPLETADA, Orden ID: {order.ref}, Asset {order.data._name}, Size {order.size}, Strategy {order.info.strategy}')
+                
+                # Se carga la cantidad de activos que compro la estrategia
                 self.positions_strategy[order.data._name] += order.size
             elif order.issell():
                 print(f'VENTA COMPLETADA, Orden ID: {order.ref}, Asset {order.data._name}, Size {order.size}, Strategy {order.info.strategy}')
+                
+                # Como se vendieron todos los activos, se coloca en 0 la cantidad
                 self.positions_strategy[order.data._name] = 0
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            print(f'ORDEN FALLIDA, Orden ID: {order.ref}')
+        elif order.status == order.Canceled:
+            print(f'ORDEN CANCELED, Orden ID: {order.ref}, Status: {order.status}')
+        elif order.status == order.Margin:
+            print(f'ORDEN MARGIN, Orden ID: {order.ref}, Size: {order.size}, Asset {order.data.close[0]}')
+        elif order.status == order.Rejected:
+            print(f'ORDEN REJECTED, Orden ID: {order.ref}, Status: {order.status}')
 
-        # Limpiar la referencia de la orden después de que se ha completado o fallado
         self.order = None
     
     def __str__(self) -> str:
